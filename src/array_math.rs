9f340f15ede29a37e2975cc461b9712263d844f4
elftausend <skatsacc.gut24@gmail.com>
2021-09-10 18:19:16 +0200

    using generic const exprs and added lin layer struct

--- src/array_math.rs
+++ src/array_math.rs
@@ -1,31 +1,19 @@
 use crate::Number;
 
-pub trait Forward<T, const C: usize, const C2: usize, const SIZE: usize> {
-    fn forward(&self, rhs: &[T; SIZE]) -> [T; C2];
+pub trait Forward<T, const C: usize, const C2: usize> {
+    fn forward(&self, rhs: &[T; C*C2]) -> [T; C2];
 }
-pub trait Transpose<T, const R: usize, const C: usize, const SIZE: usize> {
-    fn compute(&self) -> [T; SIZE];
+pub trait Transpose<T, const R: usize, const C: usize> {
+    fn compute(&self) -> [T; R*C];
 }
-/* 
-pub struct Backend;
 
-impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Forward<T, R, C, SIZE> for Backend {
-    fn sforward(lhs: [T; C], rhs: [T; SIZE]) {
-        for (idx, value) in lhs.iter().enumerate() {
-
-        }
-        todo!()
-    }
-}
-*/
-
-impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Transpose<T, R, C, SIZE> for [T; SIZE] {
-    fn compute(&self) -> [T; SIZE] {
+impl <T: Number, const R: usize, const C: usize, >Transpose<T, R, C,> for [T; R*C] {
+    fn compute(&self) -> [T; R*C] {
         let rows = R;
         let cols = C;
         let data = self;
 
-        let mut output = [T::default(); SIZE];
+        let mut output = [T::default(); R*C];
         for i in 0..rows {
             let index = i*cols;
             let row = &data[index..index+cols];
@@ -39,9 +27,9 @@ impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Transpose<T, 
     }
 }
 
-impl <T: Number, const C: usize, const C2: usize, const SIZE: usize>Forward<T, C, C2, SIZE> for [T; C] {
-    fn forward(&self, rhs: &[T; SIZE]) -> [T; C2] {
-        let rhs = Transpose::<T, C, C2, SIZE>::compute(rhs);
+impl <T: Number, const C: usize, const C2: usize, >Forward<T, C, C2,> for [T; C] {
+    fn forward(&self, rhs: &[T; C*C2]) -> [T; C2] {
+        let rhs = Transpose::<T, C, C2>::compute(rhs);
         let mut out = [T::default(); C2];
         for col in 0..C2 {
             let mut acc = T::default();
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
+#![feature(generic_const_exprs)]
 mod array_math;
+use std::time::Instant;
+
 pub use array_math::*;
 
 mod number;
 pub use number::*;
 
+mod linear_layer;
+pub use linear_layer::*;
+
 #[test]
 fn math() {
     let x = [2, 1, 3];
+    let y = [3, 1,
+                     3, 6, 
+                     5, 3];
     //x.Forward::<2>(&[9, 1, 2, 3, 2, 8]);
-    let out = Forward::<i32, 3, 2, {3*2}>::forward(&x, &[3, 1,
-                                                                          3, 6, 
-                                                                          5, 3]);
+    let before = Instant::now();
 
+    for _ in 0..1_000_000 {
+        let out = Forward::<_, 3, 2>::forward(&x, &y);
+    }
+    
+    let after = Instant::now();
+    println!("dur: {:?}", after-before);
+    let out = Forward::<_, 3, 2>::forward(&x, &y);
+    
     println!("out: {:?}", out);
 
     
