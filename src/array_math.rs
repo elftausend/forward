6143f9c2332abc6c067781c5196969f0d57616f3
elftausend <skatsacc.gut24@gmail.com>
2021-09-10 07:08:20 +0200

    added math trait and forward impl

--- src/array_math.rs
+++ src/array_math.rs
@@ -1,3 +1,31 @@
-pub trait Math<T, const C: usize, const R: usize> {
-    fn forward(lhs: []);
+use crate::Number;
+
+pub trait Forward<T, const C: usize, const C2: usize, const SIZE: usize> {
+    fn forward(&self,  rhs: &[T; SIZE]) -> [T; C2];
+}
+/* 
+pub struct Backend;
+
+impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Forward<T, R, C, SIZE> for Backend {
+    fn sforward(lhs: [T; C], rhs: [T; SIZE]) {
+        for (idx, value) in lhs.iter().enumerate() {
+
+        }
+        todo!()
+    }
+}
+*/
+
+impl <T: Number, const C: usize, const C2: usize, const SIZE: usize>Forward<T, C, C2, SIZE> for [T; C] {
+    fn forward(&self, rhs: &[T; SIZE]) -> [T; C2] {
+        let mut out = [T::default(); C2];
+        for col in 0..C2 {
+            let mut acc = T::default();
+            for idx in 0..C {
+                acc += self[idx]*rhs[col*C + idx]
+            }
+            out[col] = acc;
+        }
+        out
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ pub use array_math::*;
 
 mod number;
 pub use number::*;
+
+#[test]
+fn math() {
+    let x = [2, 1, 3];
+    //x.Forward::<2>(&[9, 1, 2, 3, 2, 8]);
+    let out = Forward::<i32, 3, 2, {2*3}>::forward(&x, &[3, 1, 3, 
+                                                                          6, 5, 3]);
+
+    println!("")                                                                        
+
+    
+}
