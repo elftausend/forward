1832b1181efc611a0e0093e6b5b6af22ccd520c9
elftausend <skatsacc.gut24@gmail.com>
2021-09-10 08:48:49 +0200

    added transpose

--- src/array_math.rs
+++ src/array_math.rs
@@ -1,7 +1,10 @@
 use crate::Number;
 
 pub trait Forward<T, const C: usize, const C2: usize, const SIZE: usize> {
-    fn forward(&self,  rhs: &[T; SIZE]) -> [T; C2];
+    fn forward(&self, rhs: &[T; SIZE]) -> [T; C2];
+}
+pub trait Transpose<T, const R: usize, const C: usize, const SIZE: usize> {
+    fn compute(&self) -> [T; SIZE];
 }
 /* 
 pub struct Backend;
@@ -16,8 +19,29 @@ impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Forward<T, R,
 }
 */
 
+impl <T: Number, const R: usize, const C: usize, const SIZE: usize>Transpose<T, R, C, SIZE> for [T; SIZE] {
+    fn compute(&self) -> [T; SIZE] {
+        let rows = R;
+        let cols = C;
+        let data = self;
+
+        let mut output = [T::default(); SIZE];
+        for i in 0..rows {
+            let index = i*cols;
+            let row = &data[index..index+cols];
+            
+            for (idx, value) in row.iter().enumerate() {
+                let idx = rows*idx+i;
+                output[idx] = *value;
+            }
+        }
+        output
+    }
+}
+
 impl <T: Number, const C: usize, const C2: usize, const SIZE: usize>Forward<T, C, C2, SIZE> for [T; C] {
     fn forward(&self, rhs: &[T; SIZE]) -> [T; C2] {
+        let rhs = Transpose::<T, C, C2, SIZE>::compute(rhs);
         let mut out = [T::default(); C2];
         for col in 0..C2 {
             let mut acc = T::default();
--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,11 @@ pub use number::*;
 fn math() {
     let x = [2, 1, 3];
     //x.Forward::<2>(&[9, 1, 2, 3, 2, 8]);
-    let out = Forward::<i32, 3, 2, {2*3}>::forward(&x, &[3, 1, 3, 
-                                                                          6, 5, 3]);
+    let out = Forward::<i32, 3, 2, {3*2}>::forward(&x, &[3, 1,
+                                                                          3, 6, 
+                                                                          5, 3]);
 
-    println!("")                                                                        
+    println!("out: {:?}", out);
 
     
 }
